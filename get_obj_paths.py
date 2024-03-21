465dfbf64b73d65fc88160e6fb552e240f6e0e25
Kevin Li <kevinli020508@gmail.com>
2024-03-21 17:33:55 -0400

    Add hat snapshots

--- get_obj_paths.py
+++ get_obj_paths.py
@@ -5,6 +5,6 @@ def get_subfolder_names(path):
     return subfolders
 
 if __name__ == "__main__":
-    path = 'objs/02942699'
+    path = 'objs/03513137'
     subfolder_names = get_subfolder_names(path)
     print('[' + ', '.join(f'"{name}"' for name in subfolder_names) + ']')
