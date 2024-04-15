f707e3800f31e21c983bdc964b84805f09a7e737
Kevin Li <kevinli020508@gmail.com>
2024-04-15 19:13:46 -0400

    Add approach=all

--- search.py
+++ search.py
@@ -5,12 +5,16 @@ from scipy.spatial.distance import cosine
 from pathlib import Path
 from collections import defaultdict
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 
 def load_embedding(file_path):
     return np.loadtxt(file_path)
 
+
 def compute_cosine_similarity(vec1, vec2):
     return 1 - cosine(vec1, vec2)
+
 
 from dataclasses import dataclass
 
@@ -27,6 +31,7 @@ class CrossModal(Approach):
 
 class ImageToText(Approach):
     pass
+
 
 def get_ranked_snapshots(query_id, k, approach):
     sketch_embedding_path = f"vertex/sketches-png_embeddings/{query_id}.npy"
@@ -70,6 +75,7 @@ def get_ranked_snapshots(query_id, k, approach):
     
     return ranked_snapshots[:k]
 
+
 def process_query_id(query_id, k, approach):
     expected_snapshot_id = query_id.split('_')[0]
     ranked_snapshots = get_ranked_snapshots(query_id=query_id, k=k, approach=approach)
@@ -78,9 +84,39 @@ def process_query_id(query_id, k, approach):
     else:
         return 0
 
+
+def eval_weighted_sum(query_ids, k):
+    weight_steps = 10
+
+    weighted_sum_top_k_hits = defaultdict(int)
+
+    for step in tqdm(range(0, weight_steps + 1)):
+        image_weight = step / weight_steps
+        text_weight = 1 - image_weight
+        from multiprocessing import Pool
+        from functools import partial
+
+        with Pool() as p:
+            weighted_sum_top_k_hits[image_weight] = sum(p.map(partial(process_query_id, k=k, approach=WeightedSum(image_weight, text_weight)), query_ids))
+    
+    return weighted_sum_top_k_hits
+
+
+def eval_single_config(query_ids, k, approach):
+    top_k_hits = 0
+
+    from multiprocessing import Pool
+    from functools import partial
+
+    with Pool() as p:
+        top_k_hits = sum(p.map(partial(process_query_id, k=k, approach=approach), query_ids))
+    
+    return top_k_hits
+
+
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--approach', type=str, default='weighted_sum', help='Approach to use for search', choices=['weighted_sum', 'crossmodal', 'image_to_text'])
+    argparser.add_argument('--approach', type=str, default='weighted_sum', help='Approach to use for search', choices=['weighted_sum', 'crossmodal', 'image_to_text', 'all'])
 
     args = argparser.parse_args()
 
@@ -93,22 +129,9 @@ def main():
             query_ids.append(Path(file).stem)
 
     if args.approach == 'weighted_sum':
-        weight_steps = 10
-
-        weighted_sum_top_k_hits = defaultdict(int)
-
-        for step in tqdm(range(0, weight_steps + 1)):
-            image_weight = step / weight_steps
-            text_weight = 1 - image_weight
-            from multiprocessing import Pool
-            from functools import partial
-
-            with Pool() as p:
-                weighted_sum_top_k_hits[image_weight] = sum(p.map(partial(process_query_id, k=k, approach=WeightedSum(image_weight, text_weight)), query_ids))
+        weighted_sum_top_k_hits = eval_weighted_sum()
 
         print(weighted_sum_top_k_hits)
-
-        import matplotlib.pyplot as plt
 
         x = list(weighted_sum_top_k_hits.keys())
         y = list(weighted_sum_top_k_hits.values())
@@ -119,17 +142,26 @@ def main():
         plt.title('Top k Hits vs Image Weight')
         plt.savefig('weighted_sum_top_k_hits.png')
     elif args.approach == 'crossmodal' or args.approach == 'image_to_text':
-        top_k_hits = 0
-
         approach = CrossModal() if args.approach == 'crossmodal' else ImageToText()
-
-        from multiprocessing import Pool
-        from functools import partial
-
-        with Pool() as p:
-            top_k_hits = sum(p.map(partial(process_query_id, k=k, approach=approach), query_ids))
-
+        top_k_hits = eval_single_config(query_ids, k, approach)
         print(f"Top k hits for {args.approach} search: {top_k_hits}")
+    elif args.approach == 'all':
+        weighted_sum_top_k_hits = eval_weighted_sum(query_ids, k)
+
+        x = list(weighted_sum_top_k_hits.keys())
+        y = [hits / len(query_ids) for hits in weighted_sum_top_k_hits.values()]
+
+        crossmodal_top_k_hits = eval_single_config(query_ids, k, CrossModal()) / len(query_ids)
+        image_to_text_top_k_hits = eval_single_config(query_ids, k, ImageToText()) / len(query_ids)
+
+        plt.plot(x, y, label='Weighted Sum')
+        plt.axhline(crossmodal_top_k_hits, linestyle='-', color='green', label='Crossmodal')
+        plt.axhline(image_to_text_top_k_hits, linestyle='-', color='red', label='Image to Text')
+        plt.xlabel('Image Weight')
+        plt.ylabel(f'Percent Top {k} Hits')
+        plt.title(f'Percent Top {k} Hits')
+        plt.legend()
+        plt.savefig(f'percent_top_{k}_hits.png')
 
 if __name__ == "__main__":
     main()
