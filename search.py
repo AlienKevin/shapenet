10a0040761730d1ae4115efaf7c53bd987d2e63c
Kevin Li <kevinli020508@gmail.com>
2024-04-15 20:08:36 -0400

    Generate azure plots

--- search.py
+++ search.py
@@ -33,16 +33,16 @@ class ImageToText(Approach):
     pass
 
 
-def get_ranked_snapshots(query_id, k, approach):
-    sketch_embedding_path = f"vertex/sketches-png_embeddings/{query_id}.npy"
+def get_ranked_snapshots(query_id, k, approach, embedding_model):
+    sketch_embedding_path = f"{embedding_model}/sketches-png_embeddings/{query_id}.npy"
     sketch_embedding = load_embedding(sketch_embedding_path)
     
-    text_embedding_path = f"vertex/ImagetoTextOutput_embeddings/{query_id}_GPT4.npy"
+    text_embedding_path = f"{embedding_model}/ImagetoTextOutput_embeddings/{query_id}_GPT4.npy"
     text_embedding = load_embedding(text_embedding_path)
     
-    snapshot_embeddings_dir = "vertex/camera_embeddings/"
-    system_text_embeddings_dir = "vertex/ImagetoTextOutput_embeddings/"
-    sketch_text_embeddings_dir = "vertex/SketchesImagetoTextOutput_embeddings/"
+    snapshot_embeddings_dir = f"{embedding_model}/camera_embeddings/"
+    system_text_embeddings_dir = f"{embedding_model}/ImagetoTextOutput_embeddings/"
+    sketch_text_embeddings_dir = f"{embedding_model}/SketchesImagetoTextOutput_embeddings/"
     similarity_scores = []
     
     for snapshot_file in os.listdir(snapshot_embeddings_dir):
@@ -76,16 +76,16 @@ def get_ranked_snapshots(query_id, k, approach):
     return ranked_snapshots[:k]
 
 
-def process_query_id(query_id, k, approach):
+def process_query_id(query_id, k, approach, embedding_model):
     expected_snapshot_id = query_id.split('_')[0]
-    ranked_snapshots = get_ranked_snapshots(query_id=query_id, k=k, approach=approach)
+    ranked_snapshots = get_ranked_snapshots(query_id=query_id, k=k, approach=approach, embedding_model=embedding_model)
     if any(snapshot_id == expected_snapshot_id for snapshot_id, _ in ranked_snapshots):
         return 1
     else:
         return 0
 
 
-def eval_weighted_sum(query_ids, k):
+def eval_weighted_sum(query_ids, k, embedding_model):
     weight_steps = 10
 
     weighted_sum_top_k_hits = defaultdict(int)
@@ -97,19 +97,19 @@ def eval_weighted_sum(query_ids, k):
         from functools import partial
 
         with Pool() as p:
-            weighted_sum_top_k_hits[image_weight] = sum(p.map(partial(process_query_id, k=k, approach=WeightedSum(image_weight, text_weight)), query_ids))
+            weighted_sum_top_k_hits[image_weight] = sum(p.map(partial(process_query_id, k=k, approach=WeightedSum(image_weight, text_weight), embedding_model=embedding_model), query_ids))
     
     return weighted_sum_top_k_hits
 
 
-def eval_single_config(query_ids, k, approach):
+def eval_single_config(query_ids, k, approach, embedding_model):
     top_k_hits = 0
 
     from multiprocessing import Pool
     from functools import partial
 
     with Pool() as p:
-        top_k_hits = sum(p.map(partial(process_query_id, k=k, approach=approach), query_ids))
+        top_k_hits = sum(p.map(partial(process_query_id, k=k, approach=approach, embedding_model=embedding_model), query_ids))
     
     return top_k_hits
 
@@ -117,18 +117,20 @@ def eval_single_config(query_ids, k, approach):
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--approach', type=str, default='weighted_sum', help='Approach to use for search', choices=['weighted_sum', 'crossmodal', 'image_to_text', 'all'])
+    argparser.add_argument('--embedding_model', type=str, default='vertex', help='Embedding model for text and image', choices=['azure', 'vertex'])
     argparser.add_argument('--k', type=int, default=10, help='Top K hits to consider for evaluation')
     args = argparser.parse_args()
 
     k = args.k
+    embedding_model = args.embedding_model
 
     query_ids = []
-    for root, dirs, files in os.walk('vertex/sketches-png_embeddings'):
+    for root, dirs, files in os.walk(f'{embedding_model}/sketches-png_embeddings'):
         for file in files:
             query_ids.append(Path(file).stem)
 
     if args.approach == 'weighted_sum':
-        weighted_sum_top_k_hits = eval_weighted_sum()
+        weighted_sum_top_k_hits = eval_weighted_sum(query_ids, k, embedding_model)
 
         print(weighted_sum_top_k_hits)
 
@@ -142,16 +144,16 @@ def main():
         plt.savefig('weighted_sum_top_k_hits.png')
     elif args.approach == 'crossmodal' or args.approach == 'image_to_text':
         approach = CrossModal() if args.approach == 'crossmodal' else ImageToText()
-        top_k_hits = eval_single_config(query_ids, k, approach)
+        top_k_hits = eval_single_config(query_ids, k, approach, embedding_model)
         print(f"Top k hits for {args.approach} search: {top_k_hits}")
     elif args.approach == 'all':
-        weighted_sum_top_k_hits = eval_weighted_sum(query_ids, k)
+        weighted_sum_top_k_hits = eval_weighted_sum(query_ids, k, embedding_model)
 
         x = list(weighted_sum_top_k_hits.keys())
         y = [hits / len(query_ids) for hits in weighted_sum_top_k_hits.values()]
 
-        crossmodal_top_k_hits = eval_single_config(query_ids, k, CrossModal()) / len(query_ids)
-        image_to_text_top_k_hits = eval_single_config(query_ids, k, ImageToText()) / len(query_ids)
+        crossmodal_top_k_hits = eval_single_config(query_ids, k, CrossModal(), embedding_model) / len(query_ids)
+        image_to_text_top_k_hits = eval_single_config(query_ids, k, ImageToText(), embedding_model) / len(query_ids)
 
         plt.plot(x, y, label='Weighted Sum')
         plt.axhline(crossmodal_top_k_hits, linestyle='-', color='green', label='Crossmodal')
@@ -160,7 +162,7 @@ def main():
         plt.ylabel(f'Percent Top {k} Hits')
         plt.title(f'Percent Top {k} Hits')
         plt.legend(loc='center left')
-        plt.savefig(f'percent_top_{k}_hits.png')
+        plt.savefig(f'percent_top_{k}_hits_{embedding_model}.png')
 
 if __name__ == "__main__":
     main()
